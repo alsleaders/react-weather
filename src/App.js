56beb7a050bab6fdfe3d16f9006d3d8077abb4e4
Amanda Leaders <alsleaders@gmail.com>
2019-05-28 17:05:07 -0400

    weather start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import WeatherSearch from './components/WeatherSearch'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <HelloWorld />
+        <WeatherSearch />
+      </>
+    )
   }
 }
 
--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,27 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+const beginningOfWeatherUrl =
+  'https://api.openweathermap.org/data/2.5/weather?q='
 
 class WeatherSearch extends Component {
+  state = {
+    value: '',
+    weather: {}
+  }
+
+  handleChange = event => {
+    console.log('are you updating value')
+    this.setState({
+      value: this.state.value
+    })
+  }
+
+  updateCity = event => {
+    console.log('take the value and make weather')
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  goGetTheWeather = () => {
+    fetch(
+      `${beginningOfWeatherUrl}${
+        this.state.value
+      }&APPID=017ef3e4bf1e11d8cf9397e2e73fd25a`
+    )
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      // then display
+      .then(city => {
+        console.log({ city })
+        console.log('is something happening?')
+      })
+  }
   render() {
     return (
       <div>
-        fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-      Tampa +
-      '&appid=017ef3e4bf1e11d8cf9397e2e73fd25a' 
-  )
-    .then(response => {
-      console.log(response)
-      return response.json()
-    })
-    // then display
-    .then(city => {
-      console.log({ city })
-      console.log(city.weather)
-      console.log(city.weather[0])
-    })
+        <p>Hallo!</p>
+        <input
+          type="text"
+          placeholder="Enter a city or zip code"
+          value={this.state.value}
+          onChange={this.updateCity}
+        />
+        <button onClick={this.goGetTheWeather}>Submit</button>
       </div>
-    );
+    )
   }
 }
 
-export default WeatherSearch;
+export default WeatherSearch
