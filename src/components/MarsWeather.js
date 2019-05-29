bea4f39ea72fd7558f09380a9422c12081f6dcf2
Amanda Leaders <alsleaders@gmail.com>
2019-05-29 09:45:20 -0400

    added mars weather

--- src/components/MarsWeather.js
+++ src/components/MarsWeather.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
 
 class MarsWeather extends Component {
+  state = {
+    date: '',
+    weather: '',
+    max: '',
+    min: ''
+  }
+  componentDidMount() {
+    console.log('component mounted')
+    this.getMarsData()
+  }
+
+  getMarsData = () => {
+    fetch('https://mars.nasa.gov/rss/api/json/msl_marsweather.json')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(data => {
+        console.log({ data })
+        console.log('did the Mars fetch work?')
+        console.log(data.soles[0].terrestrial_date)
+        console.log(data.soles[0].atmo_opacity)
+        console.log(data.soles[0].max_temp)
+        console.log(data.soles[0].min_temp)
+        this.setState({
+          date: data.soles[0].terrestrial_date,
+          weather: data.soles[0].atmo_opacity,
+          max: data.soles[0].max_temp,
+          min: data.soles[0].min_temp
+        })
+      })
+  }
   render() {
-    // componentDidMount() {
-    //   console.log('component mounted')
-    //   this.getMarsData()
-    // }
-
-    // getMarsData = () => {
-    //   fetch('https://mars.nasa.gov/rss/api/json/msl_marsweather.json')
-    //     .then(response => {
-    //       console.log(response)
-    //       return response.json()
-    //     })
-    //     .then(data => {
-    //       console.log({ data })
-    //       console.log('did the Mars fetch work?')
-    //     })
-    // }
-
     return (
-      <div>
-        In case you're wondering. The current temperature in the Gale crater on
-        Mars is <p>going to be filled in here when the fetch works</p>
-      </div>
+      <section>
+        In case you're wondering. The temperature in the Gale crater on Mars for
+        the Earth date {this.state.date} is {this.state.weather}, the high is{' '}
+        {this.state.max} and the low is {this.state.min} degrees Celsius.
+      </section>
     )
   }
 }
